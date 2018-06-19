bf01d16266f1d411204bef4c6bb33ea2c7660730
Selali Adobor <selaliadobor@github.com>
2018-06-19 17:33:02 -0400

    Add API definition and accompanying injections

---
+++ app/src/main/java/com/willowtree/namegame/api/WillowtreeApiService.java
@@ -0,0 +1,14 @@
+package com.willowtree.namegame.api;
+
+import com.willowtree.namegame.api.profiles.Profile;
+
+import java.util.List;
+
+import io.reactivex.Single;
+import retrofit2.http.GET;
+
+
+public interface WillowtreeApiService {
+        @GET("api/v1.0/profiles")
+        Single<List<Profile>> fetchAllProfiles();
+}
