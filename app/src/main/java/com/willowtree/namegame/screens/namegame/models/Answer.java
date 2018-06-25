f3b072ccd165411086727942deb8f18f60849ae7
Selali Adobor <selaliadobor@github.com>
2018-06-25 06:30:34 -0400

    Make Answer implement parcelable
    Take advantage of AutoValue-Parcel

--- app/src/main/java/com/willowtree/namegame/screens/namegame/models/Answer.java
+++ app/src/main/java/com/willowtree/namegame/screens/namegame/models/Answer.java
@@ -1,20 +1,22 @@
 package com.willowtree.namegame.screens.namegame.models;
 
+import android.os.Parcelable;
+
 import com.google.auto.value.AutoValue;
 import com.willowtree.namegame.api.profiles.Profile;
 
 @AutoValue
-public abstract class Answer {
+public abstract class Answer implements Parcelable {
 
     public boolean wasCorrect() {
-        return guessedProfile().getId().equals(challenge().correctProfileId());
+        return guessedProfileId().equals(challenge().correctProfileId());
     }
 
     public abstract Challenge challenge();
 
-    public abstract Profile guessedProfile();
+    public abstract String guessedProfileId();
 
     public static Answer create(Challenge challenge, Profile guessedProfile) {
-        return new AutoValue_Answer(challenge, guessedProfile);
+        return new AutoValue_Answer(challenge, guessedProfile.getId());
     }
 }
